90d7fa49aaaf64c3f2ed2ad090cb41b5c526e8f4
Karl Yngström <karl.yngstrom@gmail.com>
2018-09-03 13:55:49 +0200

    nt

--- SpaceInvaders/wwwroot/js/site.js
+++ SpaceInvaders/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+var canvas = document.getElementsByTagName('gameCanvas')[0];
+canvas.width = 800;
+canvas.height = 600;
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
