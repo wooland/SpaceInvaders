6b7e6a0152ada565eab065338c1292493d415612
Karl Yngström <karl.yngstrom@gmail.com>
2018-09-05 09:36:55 +0200

    nt

--- SpaceInvaders/wwwroot/js/Game.js
+++ SpaceInvaders/wwwroot/js/Game.js
@@ -4,6 +4,7 @@
 var playerOne;
 var playerTwo;
 var enemyMiniboss;
+var enemyPlanes = [];
 
 
 
@@ -17,14 +18,14 @@ function InitGame() {
     imgPlayer1.src = 'images/Sprites/Player.png';
 
     //skapa player1
-    playerOne = new airplane(40, 40, imgPlayer1, 10, 120);
+    playerOne = new airplane(30, 30, imgPlayer1, 10, 120);
 
    //nme
     var imgEnemy = new Image();
     imgEnemy.src = 'images/Sprites/Enemy.png';
     enemyMiniboss = new airplane(30, 30, imgEnemy, 800, 320);
-    enemyMiniboss.speedX = -3;
-    enemyMiniboss.speedY = 1;
+    enemyMiniboss.speedX = -1;
+    enemyMiniboss.speedY = 0;
 
 
 };
@@ -45,21 +46,20 @@ var myGameSky = {
         this.context.fillStyle = "lightblue";
         this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
-        this.interval = setInterval(updateGameArea, 50);
+        this.interval = setInterval(updateGameArea, 10);
 
 
         window.addEventListener('keydown', function (e) {
             myGameSky.keys = (myGameSky.keys || []);
             myGameSky.keys[e.keyCode] = true;
-
-           
-            console.log(myGameSky.keys[39])
-    
-
+ 
         });
 
         window.addEventListener('keyup', function (e) {
-            myGameSky.keys[e.keyCode] = false;
+            if (myGameSky.keys != null) {
+                myGameSky.keys[e.keyCode] = false;
+            }
+            
         });
         this.context = this.canvas.getContext('2d');
     },
@@ -71,20 +71,26 @@ var myGameSky = {
     },
     clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop: function () {
+        clearInterval(this.interval);
     }
 
 }
 
 function updateGameArea() {
-    
-    myGameSky.clear();
-    myGameSky.updateSky();
-    
-    playerOne.newPos();
-    playerOne.update();
+    if (playerOne.crashWith(enemyMiniboss)) {
+        myGameSky.stop();
+    } else {
+        myGameSky.clear();
+        myGameSky.updateSky();
 
-    enemyMiniboss.newPos();
-    enemyMiniboss.update();
+        playerOne.newPos();
+        playerOne.update();
+
+        enemyMiniboss.newPos();
+        enemyMiniboss.update();
+    }
 }
 
 function airplane(width, height, image, x, y) {
@@ -102,16 +108,43 @@ function airplane(width, height, image, x, y) {
         ctx.drawImage(image, this.x, this.y);
     };
     this.update = function () {
+        
         if (myGameSky.keys && myGameSky.keys[37]) { playerOne.speedX = -1; }
         if (myGameSky.keys && myGameSky.keys[39]) { playerOne.speedX = 1; }
         if (myGameSky.keys && myGameSky.keys[38]) { playerOne.speedY = -1; }
         if (myGameSky.keys && myGameSky.keys[40]) { playerOne.speedY = 1; }
+
+        if (myGameSky.keys && !myGameSky.keys[37] && !myGameSky.keys[39]) {
+            playerOne.speedX = 0;
+        }
+        if (myGameSky.keys && !myGameSky.keys[38] && !myGameSky.keys[40]) {
+            playerOne.speedY = 0;
+        }
+        
         ctx.drawImage(image, this.x, this.y);
         
     }
     this.newPos = function () {
         this.x += this.speedX;
         this.y += this.speedY;
+    }
+    this.crashWith = function (otherobj) {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height);
+        var crash = true;
+        if ((mybottom < othertop) ||
+            (mytop > otherbottom) ||
+            (myright < otherleft) ||
+            (myleft > otherright)) {
+            crash = false;
+        }
+        return crash;
     }
 
 }
