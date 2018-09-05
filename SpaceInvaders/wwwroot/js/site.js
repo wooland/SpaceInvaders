c791355dbdba840d16e387e9a2cea972be8aa530
Jonas Schultz <jonasschultz27@gmail.com>
2018-09-05 09:30:23 +0200

    nt

--- SpaceInvaders/wwwroot/js/site.js
+++ SpaceInvaders/wwwroot/js/site.js
@@ -15,23 +15,16 @@ ctx.fillRect(0, 0, canvas.width, canvas.height);
 var img1 = new Image();
 //drawing of the test image - img1
 img1.onload = function () {
-    //draw background image
-    ctx.drawImage(img1, 10, 10);
-    //draw a box over the top
-    //ctx.fillStyle = "rgba(200, 0, 0, 0.5)";
-    //ctx.fillRect(0, 0, 500, 500);
-
+    ctx.drawImage(img1, 500, 150);
 };
 
 img1.src = 'images/Sprites/Enemy.png';
-
 
 var img2 = new Image();
 
 img2.onload = function () {
     //draw background image
     ctx.drawImage(img2, 100, 100);
-
 };
 
 img2.src = 'images/Sprites/Player.png';
